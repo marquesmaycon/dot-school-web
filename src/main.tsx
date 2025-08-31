629b72bacfc4ecdbd55570a6851f596e42b13d6f
Maycon Marques <mayconmarquesh@gmail.com>
2025-08-31 12:03:10 -0300

    feat: add React Query Devtools for improved debugging

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router"
 import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 import "./index.css"
 import { queryClient } from "./lib/tanstack-query.ts"
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AppRouter />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
